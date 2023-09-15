70051a5e940355d9baf77982835e4818099c0b0a
Astha <dubeyastha210@gmail.com>
2023-09-15 20:24:11 +0530

    only index render problem

--- My project/CRUD/app/urls.py
+++ My project/CRUD/app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from app import views
 urlpatterns = [
     path('',views.index,name="index"),
-    path('about',views.index,name="about1"),
+    path('about',views.about,name="about"),
 ]
